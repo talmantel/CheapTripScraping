e871a22b26181145b762af68c32059eb0e49cb57
Ilya Soloveychik <dj.active.17@gmail.com>
2021-03-21 23:13:58 +0200

    todo: grab data properly

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 // Simple travel. In the future we can expand this script
 const csv = require('csv-parser');
-const fs = require('fs')
+const fs = require('fs');
+const { performance } = require('perf_hooks');
 const grab = require('./helpers/grab');
-const measureMS = require('./performance_tests/measureMS');
+
 
 let cities1 = cities2 = [];
 fs.createReadStream('India for scraping.csv')
   .pipe(csv())
-  .on('data', (data) => {
-    cities1.push([data.id, `${data.city},${data.country}`])
-    cities2.push([data.id, `${data.city},${data.country}`])
+  .on('data', (data) => { // -800 india
+    cities1.push([parseInt(data.id)-800, `${data.city},${data.country}`]);
+    //cities2.push([parseInt(data.id)-800, `${data.city},${data.country}`]);
   })
   .on('end', () => {
+    const measureMS = require('./performance_tests/measureMS');
     let t0 = performance.now();
-      
+    console.log(cities1); //, cities2);
     let from = to = '';
-    for (let i = 0; i < cities1.length; i++){
-      for (let j = 1; j < cities2.length - 1; j++){
+    for (let i = 0; i < cities1.length; i += 2){
+      for (let j = 1; j < cities2.length - 1; j += 2){
         if (cities1[i] !== cities2[j]){
           grab({
             id: cities1[i][0],
--- performance_tests/measureMS.js
+++ performance_tests/measureMS.js
@@ -1,23 +1,23 @@
-// Shulamit
+// Shulamit's func :)
 const fs = require('fs');
 const { performance } = require('perf_hooks');
 
-function millisToMinutesAndSeconds(millis) {
-    var minutes = Math.floor(millis / 60000);
-    var seconds = ((millis % 60000) / 1000).toFixed(0);
-    return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-  }
+const millisToMinutesAndSeconds = (millis) => {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+}
 
-module.exports = {
-  measureMs: (operationName, time) => {
-    const now = performance.now();
-    const timeOfOperation = now - time;
-    const durationParsed = millisToMinutesAndSeconds(timeOfOperation);
-    fs.writeFile(__dirname + `/${operationName}.txt`, `measureMs -> time for operation of "${
-      operationName || 'operation'
-    }": ${durationParsed}\n`, { flag: 'a+' }, (e) => {
-      console.log(e);
-    });
-  }
-};
+const measureMs = (operationName, time) => {
+  const now = performance.now();
+  const timeOfOperation = now - time;
+  const durationParsed = millisToMinutesAndSeconds(timeOfOperation);
+  fs.writeFile(__dirname + '/performance.txt', `measureMs -> time for operation of "${
+    operationName || 'operation'
+  }": ${durationParsed}\n`, { flag: 'a+' }, (e) => {
+    console.log(e);
+  });
+}
+
+module.exports = measureMs;
 

e0e3331b6627c36e7db8dc62523046fa08621fb9
Ilya Soloveychik <dj.active.17@gmail.com>
2021-03-24 22:57:18 +0200

    fix delay bug: todo single zipped file

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ fs.createReadStream(process.argv[2])
     try {
       let t0 = performance.now();
       let from = to = '';
-      for (let i = 1; i < 10; i++){
-        for (let j = 1; j < 10; j++){
+      for (let i = 1; i < cities1.length; i++){
+        for (let j = 1; j < cities2.length; j++){
           if (cities1[i][0] !== cities2[j][0]){
             grab({
               from: cities1[i][1],

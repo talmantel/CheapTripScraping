938fe4a0c0746c776e830d1971a538bedd9cea6c
Ilya Soloveychik <dj.active.17@gmail.com>
2021-03-27 21:22:35 +0300

    grab function seems to work ok

--- README.md
+++ README.md
@@ -1,12 +1,7 @@
 # Warning
 node version must be 14.x.x or lower!
 
-If we could replace `exists` function in `helpers/putToFile.js` then we could run app on higher node version.
-
-# Run
-```
-node index.js <path_to_file.csv> [<delay>]
-```
-
-*delay* - define how much time program will sleep before it writes & zips parsed data, in milliseconds, 3000 by default. Delay is required because of async nature of node.js...
+# Working files in this branch
+`index.js`
+`helpers/grab.js`
 
--- helpers/grab.js
+++ helpers/grab.js
@@ -1,40 +1,57 @@
-const cheerio = require('cheerio');
-const got = require('got');
+const fs = require('fs');
 const { performance } = require('perf_hooks');
-const logMissedFile = require('../performance_tests/logMissedFile');
 const writeTable = require('./writeTable');
+const https = require('https'); // native node.js module for HTTP requests
 
-const grab = async (params) => {
+
+const grab = (params) => {
     const {from, from_id, to, to_id} = params;
-    const url = `https://www.rome2rio.com/map/${from}/${to}`;
 	
-	await got(url).then(response => {
-        try {
-            const measureMs = require('../performance_tests/measureMS');
-            let t1 = performance.now();
-            const $ = cheerio.load(response.body);
-            const result = $('#deeplinkTrip')[0].attribs.content;
-  
-            if (!result){
-              console.error('Error retrieving connection');
-              return;
-            }
-            measureMs('fetch rom2rio', t1);
-  
-            const fileName = `${from_id}_${from}_${to_id}_${to}.json`;
-            let t2 = performance.now();
-            writeTable(result, fileName);
-            measureMs('write and zip', t2);
- 
-            return result;
-        } catch (error) {
-          logMissedFile();
-          console.log(error, 'grabTest.js got');
-        }
-
-      }).catch(err => { throw err; });
-
+    const options = {
+      hostname: 'www.rome2rio.com',
+      port: 443,
+      path: `/map/${from}/${to}`,
+      method: 'GET'
+    };
     
+    const req = https.request(options, res => {
+      
+      res.on('data', d => {
+        fs.writeFileSync('tmp.txt', d, {flag: 'a'}); // data is fetched several times => need append flag
+      });
+
+      res.on('end', () => {
+        console.log('Fetch data ok');
+        
+        let data = fs.readFileSync('tmp.txt', 'utf-8');
+        data = data.toString();
+
+        // clean everything we don't need in received HTML
+
+        // Define text that will include JSON content only
+        let substrBegin = '<meta id=\'deeplinkTrip\' content=\'';
+        let substrBeginIndex = data.indexOf(substrBegin);
+
+        let substrEnd = '<meta id=\'deeplinkQuery\'';
+        let substrEndIndex = data.indexOf(substrEnd);
+
+        // Fine tune text grabbing indexes
+        substrBeginIndex = substrBeginIndex + 33;
+        substrEndIndex = substrEndIndex - 6;
+
+        data = data.slice(substrBeginIndex, substrEndIndex);
+        
+        fs.writeFileSync(`tables/${from_id}_${from}_${to_id}_${to}.json`, data, {flag: 'w+'});
+
+        // Important! We need to delete temporary file before new grabbing
+        fs.rmSync('tmp.txt');
+      });
+
+    });
+    
+    req.on('error', error => { throw new Error(error) });
+    
+    req.end();    
 };
 
 module.exports = grab;

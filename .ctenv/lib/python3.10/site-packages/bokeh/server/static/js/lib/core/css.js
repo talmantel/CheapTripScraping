7e49a2518afb268f4509b50afc33bb1b5e654e7d
udawe78 <udawe78@gmail.com>
2023-02-14 07:14:53 +0200

    gitignore

--- .ctenv/lib/python3.10/site-packages/bokeh/server/static/js/lib/core/css.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=css.js.map
--- .ctenv/lib/python3.10/site-packages/zope/i18n/_compat.py
+++
@@ -1,8 +0,0 @@
-# This gives a linting error because unicode is not defined on Python 3:
-# text_type = str if bytes is not str else unicode
-try:
-    # Python 2
-    text_type = unicode
-except NameError:
-    # Python 3
-    text_type = str
